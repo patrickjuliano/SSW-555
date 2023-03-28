9bee8576ac9d6e0b7ad635f4d0b597e81770ad83
snipasid <68506987+snipasid@users.noreply.github.com>
2023-03-28 18:56:45 -0400

    Create subtasks.spec.js
    Test functions for subtasks

---
+++ server/tests/subtasks.spec.js
@@ -0,0 +1,54 @@
+let chai = require('chai');
+const dbConnection = require('../config/mongoConnection');
+const data = require('../data');
+const userData = data.users;
+const projectData = data.projects;
+const taskData = data.tasks;
+const subtaskData = data.subtasks;
+
+describe('Test subtask functionality', () => {
+    let db;
+    let user;
+    let project;
+    let task;
+    const description1 = 'This is a subtask created for testing purposes';
+    let subtask1;
+    const description2 = 'This is another subtask created for testing purposes';
+    let subtask2;
+
+    before('Connect to database', async () => {
+        db = await dbConnection.dbConnection();
+        await db.dropDatabase();
+
+        user = await userData.createUser('John', 'Doe', 'john.doe@gmail.com', 'jdoe123', 'jdoe123', 'Construction Manager');
+        project = await projectData.createProject(user._id, 'Test Project');
+        task = await taskData.createTask(project._id, 'Test Task', 'This is a task created for testing purposes');
+        subtask1 = await subtaskData.createSubtask(task._id, description1);
+        subtask2 = await subtaskData.createSubtask(task._id, description2);
+    });
+
+    describe('Test getSubtask', () => {
+        it('Get subtask successfully', async () => {
+            const newSubtask1 = await subtaskData.getSubtask(task._id, subtask1._id);
+            const newSubtask2 = await subtaskData.getSubtask(task._id, subtask2._id);
+            chai.expect(newSubtask1).to.have.property('_id', subtask1._id);
+            chai.expect(newSubtask1).to.have.property('description', description1);
+            chai.expect(newSubtask2).to.have.property('_id', subtask2._id);
+            chai.expect(newSubtask2).to.have.property('description', description2);
+        });
+    });
+
+    describe('Test getAllSubtasks', () => {
+        it('Get all subtasks successfully', async () => {
+            const subtasks = await subtaskData.getAllSubtasks(task._id);
+            chai.expect(subtasks[0]).to.have.property('_id', subtask1._id);
+            chai.expect(subtasks[0]).to.have.property('description', description1);
+            chai.expect(subtasks[1]).to.have.property('_id', subtask2._id);
+            chai.expect(subtasks[1]).to.have.property('description', description2);
+        });
+    });
+
+    after('Close connection', async () => {
+        // await dbConnection.closeConnection();
+    });
+});
