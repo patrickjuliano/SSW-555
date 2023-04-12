d6a14de476e1703a1aa7cf385b2a60373c94ff72
Patrick Juliano <pjuliano15@gmail.com>
2023-04-12 16:53:59 -0400

    Clean up subproject selection transition

--- client/src/components/Subprojects.js
+++ client/src/components/Subprojects.js
@@ -15,17 +15,19 @@ const Subprojects = ({ subprojects, whitelist, open, onClose, refetch }) => {
 		<Dialog open={open} onClose={onClose} fullWidth maxWidth="xs">
             <DialogTitle>Subprojects</DialogTitle>
             <DialogContent>
-            	<List>
-					{subprojects.map((subproject, index) => (
-						<Tooltip title={!whitelist.includes(subproject._id) ? 'You do not have permission to view this project.' : ''} followCursor>
-							<span>
-								<ListItemButton onClick={() => redirect(subproject._id)} disabled={!whitelist.includes(subproject._id)}>
-									<ListItemText primary={subproject.title} />
-								</ListItemButton>
-							</span>
-						</Tooltip>
-					))}
-				</List>
+				{open &&
+					<List>
+						{subprojects.map((subproject, index) => (
+							<Tooltip title={!whitelist.includes(subproject._id) ? 'You do not have permission to view this project.' : ''} followCursor>
+								<span>
+									<ListItemButton onClick={() => redirect(subproject._id)} disabled={!whitelist.includes(subproject._id)}>
+										<ListItemText primary={subproject.title} />
+									</ListItemButton>
+								</span>
+							</Tooltip>
+						))}
+					</List>
+				}
             </DialogContent>
             <DialogActions>
 				<Button onClick={onClose}>Close</Button>
