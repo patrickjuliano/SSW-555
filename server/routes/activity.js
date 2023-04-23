4a4adb621140df3ed3924dfbc74025e432f5f6d0
csteccat <csteccat@stevens.edu>
2023-04-23 13:27:21 -0400

    Add Create routes to Activity Log

--- server/routes/activity.js
+++ server/routes/activity.js
@@ -33,4 +33,53 @@ router.get('/projects/:projectId', async (req, res) => {
     }
 });
 
+router.post('/projects/:projectId', async (req, res) => {
+    if (!req.session.userId) return res.status(401).json({error: 'You are not logged in'});
+    try {
+        req.params.projectId = validation.checkId(req.params.projectId);
+        req.query.content = validation.checkString(req.query.content);
+    } catch (e) {
+        return res.status(400).json({error: e});
+    }
+    try {
+        const message = await activityData.createMessageFromProject(req.params.projectId, req.session.userId, req.query.content);
+        return res.status(200).json(message);
+    } catch (e) {
+        return res.status(404).json({error: e});
+    }
+});
+
+router.post('/tasks/:taskId', async (req, res) => {
+    if (!req.session.userId) return res.status(401).json({error: 'You are not logged in'});
+    try {
+        req.params.taskId = validation.checkId(req.params.taskId);
+        req.query.content = validation.checkString(req.query.content);
+    } catch (e) {
+        return res.status(400).json({error: e});
+    }
+    try {
+        const message = await activityData.createMessageFromTask(req.params.taskId, req.session.userId, req.query.content);
+        return res.status(200).json(message);
+    } catch (e) {
+        return res.status(404).json({error: e});
+    }
+});
+
+router.post('/subtasks/:taskId/:subtaskId', async (req, res) => {
+    if (!req.session.userId) return res.status(401).json({error: 'You are not logged in'});
+    try {
+        req.params.taskId = validation.checkId(req.params.taskId);
+        req.params.subtaskId = validation.checkId(req.params.subtaskId);
+        req.query.content = validation.checkString(req.query.content);
+    } catch (e) {
+        return res.status(400).json({error: e});
+    }
+    try {
+        const message = await activityData.createMessageFromSubtask(req.params.taskId, req.params.subtaskId, req.session.userId, req.query.content);
+        return res.status(200).json(message);
+    } catch (e) {
+        return res.status(404).json({error: e});
+    }
+});
+
 module.exports = router;
